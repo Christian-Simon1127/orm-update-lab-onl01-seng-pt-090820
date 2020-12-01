8e2441612a4aeb4a04ad799739f5812cbc592dc1
Christian Simon <csimon1127@gmail.com>
2020-12-01 22:54:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,8 +54,8 @@ class Student
     end
   end 
   
-  def self.create(attributes)
-    student = Student.new(attributes[:name], attributes[:grade])
+  def self.create(name, grade)
+    student = Student.new(name, grade)
     student.save
     student
   end
